ada665c1ad26a0a82f6a4955c834916b7383b174
Kelly Pitella <kellypitella@gmail.com>
2025-09-30 11:59:24 -0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void hello()
+        {
+            Console.WriteLine("Hello, World!");
+        }        
+    }
+}
